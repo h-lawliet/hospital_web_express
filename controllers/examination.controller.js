97b7e5ad8c6ad757f43d7b4b23de711b1fe93d2e
h-lawliet <whoish5252@naver.com>
2025-05-08 01:22:04 +0900

    1

--- controllers/examination.controller.js
+++ controllers/examination.controller.js
@@ -14,28 +14,28 @@ export const getExaminationDetail = async (req, res) => {
   try {
     const id = req.params.id
     const ExaminationData = await Examination.findById(id)
-    res.json(ExaminationData);
+    res.json({status: 200, content: ExaminationData})
   } catch (error) {
-    res.status(500).json({ message: "서버 오류", error });
+    res.status(500).json({ message: "서버 오류", error })
   }
 }
 
 export const createExamination = async (req, res) => {
   try {
-    const { title, room, purpose, method, time, caution, result } = req.body;
-    const image = req.file?.location; // 이미지가 없을 수도 있음
+    const { title, room, purpose, method, time, caution, result } = req.body
+    const image = req.file?.location // 이미지가 없을 수도 있음
 
     if (title && room) {
-      const newExamination = new Examination({ title, room, image, purpose, method, time, caution, result });
-      const savedExamination = await newExamination.save();
-      res.status(200).json({ state: 0, message: "검사안내가 등록되었습니다" });
-      console.log(savedExamination);
+      const newExamination = new Examination({ title, room, image, purpose, method, time, caution, result })
+      const savedExamination = await newExamination.save()
+      res.json({ status: 200, message: "검사안내가 등록되었습니다" })
+      console.log("새 검사항목 : ", savedExamination)
     } else {
-      res.json({ state: 1, message: "필수항목을 모두 입력해주세요" });
+      res.json({ status: 400, message: "필수항목을 모두 입력해주세요" })
     }
   } catch (error) {
-    console.error(error);
-    res.json({ state: 2, message: "서버 오류", error });
+    console.error(error)
+    res.json({ status: 500, message: "서버 오류", error })
   }
 }
 
@@ -47,18 +47,18 @@ export const updateExamination = async (req, res) => {
 
     const examination = await Examination.findById(id)
     if (!examination) {
-      return res.status(404).json({ state: 1, message: "검사안내를 찾을 수 없습니다" })
+      return res.json({ status: 404, message: "검사안내를 찾을 수 없습니다" })
     }
 
     const newExamination = { title, room, purpose, method, time, caution, result }
     if (image) newExamination.image = image
 
     const updatedExamination = await Examination.findByIdAndUpdate(id, newExamination, { new: true })
-    res.status(200).json({ state: 0, message: "검사안내가 수정되었습니다" })
+    res.json({ status: 200, message: "검사안내가 수정되었습니다" })
     console.log("검사안내 수정 : ", updatedExamination)
   } catch (error) {
     console.error(error)
-    res.json({ state: 2, message: "서버 오류", error })
+    res.json({ status: 500, message: "서버 오류", error })
   }
 }
 
@@ -66,9 +66,9 @@ export const deleteExamination = async (req, res)=>{
   try {
     const id = req.params.id
     await Examination.findByIdAndDelete(id)
-    res.status(200).json({ state: 0, message: "검사안내가 삭제되었습니다" })
+    res.json({ status: 200, message: "검사안내가 삭제되었습니다" })
   } catch (error) {
     console.log(error)
-    res.json({ state: 2, message: "서버 오류", error })
+    res.json({ status: 500, message: "서버 오류", error })
   }
 }
